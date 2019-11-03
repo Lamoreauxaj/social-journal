4bdff665a1f0dafc594165e27baa401220d2d097
Palash Taneja <tanejapalash@gmail.com>
2019-11-03 01:02:00 -0600

    added style to a button

--- src/components/MyJournal.js
+++ src/components/MyJournal.js
@@ -21,7 +21,7 @@ class MyJournal extends Component {
   render() {
     return (
       <PageLayout className="myJournal" title="My Journal">
-        <button onClick={this.onNewEntry} className="newEntryButton btn light-green darken-2">Write a new entry</button>
+        <button onClick={this.onNewEntry} className="newEntryButton btn green-btn light-green darken-2">Write a new entry</button>
         <h5>My Entries</h5>
         {this.renderPosts()}
       </PageLayout>

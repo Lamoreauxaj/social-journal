bd2267e3c062c226605c6f05d55e159feca0e9cf
Palash Taneja <tanejapalash@gmail.com>
2019-11-03 01:59:34 -0500

    added suggestion buttons

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -12,13 +12,17 @@ class Journal extends Component {
     this.onChange = this.onChange.bind(this);
   }
 
-  
+  renderSuggestions() {
+    var suggestions = ["I worked for 2 hours on my business today", "I went for a run in the morning"];
+    return suggestions.map(suggestion => <button className="btn gray-btn saveButton">{suggestion}</button>);
+  }
 
   render() {
     return (
       <PageLayout title="Journal">
         <JournalEntry write prompt="Type a short journal entry below." text={this.state.entry} onChange={this.onChange}/>
-        <button className="btn saveButton light-green darken-2" onClick={this.onSaveEntry}>Save Entry</button>
+        <button className="btn green-btn saveButton light-green darken-2" onClick={this.onSaveEntry}>Save Entry</button>
+        {this.renderSuggestions()}
       </PageLayout>
     );
   }
